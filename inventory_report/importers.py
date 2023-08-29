14d0e8003eaac14b3ea7204c3116ec61a34d8450
Karina Gonzalez <karinagonzalez99@gmail.com>
2023-08-29 16:26:24 -0300

    Requisito 3

--- inventory_report/importers.py
+++ inventory_report/importers.py
@@ -1,20 +1,27 @@
-from typing import Dict, Type
+from abc import ABC, abstractmethod
+from typing import Dict, List, Type
+
+from inventory_report.product import Product
 
 
-class Importer:
+class Importer(ABC):
+    def __init__(self, path: str):
+        self.path = path
+
+    @abstractmethod
+    def import_data(self) -> List[Product]:
+        pass
+
+
+class JsonImporter(Importer):
     pass
 
 
-class JsonImporter:
-    pass
-
-
-class CsvImporter:
+class CsvImporter(Importer):
     pass
 
 
 # Não altere a variável abaixo
-
 IMPORTERS: Dict[str, Type[Importer]] = {
     "json": JsonImporter,
     "csv": CsvImporter,
